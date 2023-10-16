8b674f3067eb016b6720351cb44e9fe62b64a4b6
MarcosAndradeV <mastermarcos1212@hotmail.com>
2023-10-16 09:59:57 -0300

    aliases update

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -4,7 +4,7 @@ alias la='exa --icons -a'
 alias bat="bat --style=auto"
 alias c="clear"
 alias ps='procs'
-alias update='sudo yay -Syyuu'
+alias update='yay -Syyuu'
 alias erc='nvim ~/.zshrc'
 alias rld='source ~/.zshrc'
 alias ..='cd ..'
